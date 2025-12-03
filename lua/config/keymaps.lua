aaaff44f2f9781b2cb61f6802bb2270d96473793
bnamayandev <benjaminnamayandeh@gmail.com>
2025-12-03 12:13:30 -0500

    removed toggleterm because it's slow and I don't like it

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,12 @@
 vim.keymap.set("n", "<leader>gv", ":vsplit<CR>", { desc = "Vertical split" })
 
+-- open terminal
+vim.keymap.set("n", "<leader>t", function()
+	vim.cmd("vsplit | term")
+end)
+-- In terminal mode, Ctrl-q closes the terminal window instantly
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { silent = true })
+
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
