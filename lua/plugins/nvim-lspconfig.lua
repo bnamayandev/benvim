82878ddce4448999f76bd1c1d2c2a05f8b8b4e76
bnamayandev <benjaminnamayandeh@gmail.com>
2026-06-14 20:03:16 -0400

    Made lspdefinitions automatically attach to <leader>gd, so you go to the library and package definitions

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -4,25 +4,30 @@ return {
 	config = function()
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		local on_attach = function(client, bufnr)
-			client.server_capabilities.semanticTokensProvider = nil
-
-			local map = function(lhs, rhs, desc)
-				vim.keymap.set("n", lhs, rhs, { buffer = bufnr, desc = desc })
-			end
-
-			map("gd", function() Snacks.picker.lsp_definitions() end, "Goto definition")
-			map("K", vim.lsp.buf.hover, "Hover")
-			map("gr", vim.lsp.buf.references, "References")
-			map("<leader>rn", vim.lsp.buf.rename, "Rename")
-		end
-
 		vim.lsp.config("*", {
 			capabilities = capabilities,
-			on_attach = on_attach,
 		})
 
-		-- Make `gd` navigate to actual library source instead of stub (.pyi) files
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspKeymaps", { clear = true }),
+			callback = function(args)
+				local bufnr = args.buf
+				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				if client then
+					client.server_capabilities.semanticTokensProvider = nil
+				end
+
+				local map = function(lhs, rhs, desc)
+					vim.keymap.set("n", lhs, rhs, { buffer = bufnr, desc = desc })
+				end
+
+				map("gd", function() Snacks.picker.lsp_definitions() end, "Goto definition")
+				map("K", vim.lsp.buf.hover, "Hover")
+				map("gr", vim.lsp.buf.references, "References")
+				map("<leader>rn", vim.lsp.buf.rename, "Rename")
+			end,
+		})
+
 		vim.lsp.config("pyright", {
 			settings = {
 				python = {
@@ -31,6 +36,27 @@ return {
 						autoSearchPaths = true,
 						diagnosticMode = "openFilesOnly",
 					},
+				},
+			},
+		})
+
+		vim.lsp.config("clangd", {
+			cmd = {
+				"clangd",
+				"--background-index",
+				"--clang-tidy",
+				"--header-insertion=iwyu",
+				"--completion-style=detailed",
+				"--function-arg-placeholders",
+				"--query-driver=/usr/bin/gcc,/usr/bin/g++,/usr/bin/clang,/usr/bin/clang++",
+			},
+		})
+
+		vim.lsp.config("rust_analyzer", {
+			settings = {
+				["rust-analyzer"] = {
+					cargo = { allFeatures = true },
+					checkOnSave = true,
 				},
 			},
 		})
