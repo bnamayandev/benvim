5337adfe602f118578354ed283bd1b39e434a900
bnamayandev <benjaminnamayandeh@gmail.com>
2026-07-14 23:07:22 -0400

    Updated flyer to close on snack search

--- lua/plugins/fyler.lua
+++ lua/plugins/fyler.lua
@@ -33,14 +33,63 @@ return {
 			},
 		})
 
+		local opts = { kind = "split_left_most" }
+
+		local function close_finder(attempt)
+			local finder = require("fyler.finder")
+			local inst = finder.instance_get_or_nil()
+			if not inst then
+				return
+			end
+
+			if inst._is_refreshing and (attempt or 0) < 50 then
+				vim.defer_fn(function()
+					close_finder((attempt or 0) + 1)
+				end, 20)
+				return
+			end
+
+			inst:close()
+		end
+
+		local function open_finder()
+			local finder = require("fyler.finder")
+			local inst = finder.instance_get(nil, opts)
+			inst._is_refreshing = false
+			inst._refresh_again_with_args = nil
+			inst:open()
+		end
+
+		vim.api.nvim_create_autocmd("BufWinEnter", {
+			group = vim.api.nvim_create_augroup("FylerCloseOnFileOpen", { clear = true }),
+			callback = function(args)
+				local inst = require("fyler.finder").instance_get_or_nil()
+				if not inst then
+					return
+				end
+
+				local win = vim.api.nvim_get_current_win()
+				if win == inst.win_id then
+					return
+				end
+				if vim.api.nvim_win_get_config(win).relative ~= "" then
+					return
+				end
+				if vim.bo[args.buf].buftype ~= "" or vim.api.nvim_buf_get_name(args.buf) == "" then
+					return
+				end
+
+				vim.schedule(function()
+					close_finder()
+				end)
+			end,
+		})
+
 		vim.keymap.set("n", "<leader>e", function()
 			if vim.bo.filetype == "fyler_finder" then
-				local inst = require("fyler.finder").instance_get_or_nil()
-				if inst then
-					inst:close()
-				end
+				close_finder()
 			else
-				fyler.open({ kind = "split_left_most" })
+				open_finder()
 			end
 		end, {
 			desc = "Open Fyler",
